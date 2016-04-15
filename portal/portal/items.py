18d63f153996435c4f89fb716441cc1e6e3e29fb
luis rumillanca <luisrumillancaortiz@gmail.com>
2016-04-15 16:46:29 -0300

    get info from portalinmobiliario

--- portal/portal/items.py
+++ portal/portal/items.py
@@ -9,7 +9,16 @@ import scrapy
 
 
 class PortalItem(scrapy.Item):
-    # define the fields for your item here like:
-    name = scrapy.Field()
-    url = scrapy.Field()
+    inmo_name = scrapy.Field()
+    inmo_url_view = scrapy.Field()
+    inmo_url = scrapy.Field()
+    commune_name = scrapy.Field()
+    commune_url = scrapy.Field()
+    commune_total_ads = scrapy.Field()
+    pass
+
+class PortalInfo(scrapy.Item):
+    inmo_name = scrapy.Field()
+    inmo_url_view = scrapy.Field()
+    inmo_contact = scrapy.Field()
     pass
--- portal/portal/spiders/portalinfo.py
+++ portal/portal/spiders/portalinfo.py
@@ -3,11 +3,11 @@ import scrapy
 import math
 from scrapy.selector import Selector
 from scrapy.http import Request
-from portal.items import PortalItem 
+from portal.items import PortalInfo 
 
 
-class PortalinmobiliarioSpider(scrapy.Spider):
-    name = "portalinmobiliario"
+class PortalinfoSpider(scrapy.Spider):
+    name = "portalinfo"
     allowed_domains = ["portalinmobiliario.com"]
     start_urls = (
         'http://www.portalinmobiliario.com/empresas/corredoraspresentes.aspx',
@@ -27,38 +27,14 @@ class PortalinmobiliarioSpider(scrapy.Spider):
             inmo_view =  constructora.css('a::attr(href)').extract()[0]
             inmo_name = constructora.css("a img::attr(title)").extract()[0]
             inmo_url_view = "http://www.portalinmobiliario.com" + inmo_view
-            inmo_url = "http://www.portalinmobiliario.com/propiedades/broker_fic.asp?" + inmo_view.split("?")[1]
-            yield Request(inmo_url, meta={'name': inmo_name, 'url_view': inmo_url_view, 'url': inmo_url}, callback = self.parseView)
+            yield Request(inmo_url_view, meta={'name': inmo_name, 'url_view': inmo_url_view}, callback = self.parseView)
             
     def parseView(self, response):
-        inmo_name = response.meta['name']
-        inmo_url_view = response.meta['url_view']
-        inmo_url = response.meta['url']
-        for commune in response.css("table td [href*='Buscar_resp']"):
-            commune_name = commune.css('a::text').extract()[0]
-            commune_url =  commune.css('a::attr(href)').extract()[0]
-            commune_url = "http://www.portalinmobiliario.com" + commune_url.replace("..", "")
-            data = {
-                    'name': inmo_name, 
-                    'inmo_url_view': inmo_url_view,
-                    'inmo_url': inmo_url,
-                    'commune': commune_name, 
-                    'commune_url': commune_url
-            }
-            yield Request(commune_url, meta=data, callback = self.parseCommune)
-
-    def parseCommune(self, response):
-        num_items = response.css("#tableListadoPropiedades .RGBPaginacionFilaGris > td:nth-child(1) > b::text").extract()[0]
-        num_items = num_items.replace("\r\n", "")
-        num_items = num_items.replace(",", "")
-        totals = [int(s) for s in num_items.split() if s.isdigit()]
-        item = PortalItem()
+        contact = response.css(".Valor::text").extract()
+        item = PortalInfo()
         item["inmo_name"] = response.meta['name']
-        item["inmo_url_view"] = response.meta['inmo_url_view']
-        item["inmo_url"] = response.meta['inmo_url']
-        item["commune_name"] = response.meta['commune']
-        item["commune_url"] = response.meta['commune_url']
-        item["commune_total_ads"] = totals[0]
+        item["inmo_url_view"] = response.meta['url_view']
+        item["inmo_contact"] = contact
         return item
 
 
